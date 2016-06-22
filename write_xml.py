008a0251a623b04e8371eb8688b80196374cf1a6
Amos Manneschmidt <amoose136@gmail.com>
2016-06-22 16:54:41 -0400

    some things finally working: abundance grouping works at expense of redundant meshes

--- write_xml.py
+++ write_xml.py
@@ -30,8 +30,7 @@ except ImportError:
 
 import h5py
 import numpy as np
-
-
+import re
 
 filename='chimera_00774_grid_1_01.h5'
 hf = h5py.File(filename,'r')
@@ -41,14 +40,35 @@ dims.append(str(hf['mesh']['array_dimensions'][0])) #dims[0]=542
 #appends the first element of 'array_dimensions' -> 180
 dims.append(str(hf['mesh']['array_dimensions'][1])) #dims[1]=180
 dimstr = str(dims[1])+" "+str(dims[0]) #dimstr="180 542"
-grid = et.Element("Grid",Name="Abundance",GridType="Uniform")
-et.SubElement(grid,"Topology",Reference="/XDMF/Domain/Grid[1]/Topology[1]")
-et.SubElement(grid,"Grid",Reference="/XDMF/Domain/Grid[1]/Geometry[1]")
-for i,name in enumerate(hf['abundance']['a_name'][:5]):
-	attribute=et.SubElement(grid,"Attribute",Name=name,AttributeType="Scalar",Center="Cell")
+# create xdmf element
+
+# create Domain element
+xdmf = et.Element("Xdmf",Version="2.0")
+domain = et.SubElement(xdmf,"Domain")
+
+grid = {'Chimera':et.SubElement(domain,"Grid",Name="Chimera"),'Abundance':et.SubElement(domain,"Grid",Name="Abundance")}
+et.SubElement(grid['Chimera'],"Topology",TopologyType="2DRectMesh",NumberOfElements="181 543")
+geometry = et.SubElement(grid['Chimera'],"Geometry",GeometryType="VXVY")
+et.SubElement(geometry,"DataItem",Dimensions="543",NumberType="Float",Precision="8",Format="HDF").text = filename + ":/mesh/x_ef"
+et.SubElement(geometry,"DataItem",Dimensions="181",NumberType="Float",Precision="8",Format="HDF").text = filename + ":/mesh/y_ef"
+et.SubElement(grid['Chimera'],"Time",Value=str(hf['mesh']['time'].value-hf['mesh']['t_bounce'].value))
+et.SubElement(grid['Abundance'],"Topology",Reference="/Xdmf/Domain/Grid[1]/Topology[1]")
+et.SubElement(grid['Abundance'],"Geometry",Reference="/Xdmf/Domain/Grid[1]/Geometry[1]")
+
+for i,name in enumerate(hf['abundance']['a_name']):
+	if re.findall('\D\d',name):
+		element_name=re.sub('\d','',name)
+		name=re.sub('\D','',name) #find the transition between elements name and number
+		if not grid.has_key('Abundance'+'/'+element_name):
+			grid['Abundance'+'/'+element_name]=et.SubElement(domain,"Grid",Name='Abundance'+'/'+element_name,GridType="Uniform")
+			et.SubElement(grid['Abundance'+'/'+element_name],"Topology",Reference="/Xdmf/Domain/Grid[1]/Topology[1]")
+			et.SubElement(grid['Abundance'+'/'+element_name],"Geometry",Reference="/Xdmf/Domain/Grid[1]/Geometry[1]")
+		attribute=et.SubElement(grid['Abundance'+'/'+element_name],"Attribute",Name=name,AttributeType="Scalar",Center="Cell")
+	else:
+		attribute=et.SubElement(grid['Abundance'],"Attribute",Name=name,AttributeType="Scalar",Center="Cell")
 	dataElement = et.SubElement(attribute,"DataItem", ItemType="HyperSlab", Dimensions=dimstr, Type="HyperSlab")
-	et.SubElement(dataElement,"DataItem",Dimensions="3 4",Format="XML").text="\n0 0 0 "+str(i)+"\n1 1 1 1\n1 "+dimstr+" 1\n"
-	et.SubElement(dataElement,"DataItem",Dimensions="1 "+dimstr+" 161",Precisions="8",Format="HDF").text="\n"+filename+":/abundance/xn_c\n"
+	et.SubElement(dataElement,"DataItem",Dimensions="3 4",Format="XML").text="0 0 0 "+str(i)+" 1 1 1 1 1 "+dimstr+" 1"
+	et.SubElement(dataElement,"DataItem",Dimensions="1 "+dimstr+" 161",Precisions="8",Format="HDF").text=filename+":/abundance/xn_c"
 f=open(filename[:-3]+'.xmf','w')
-f.write(et.tostring(grid,pretty_print=True,xml_declaration=True))
+f.write(et.tostring(xdmf,pretty_print=True,xml_declaration=True,doctype="<!DOCTYPE Xdmf SYSTEM \"Xdmf.dtd\" []>"))
 print("--- XMF file created in %s seconds ---" % (time.time()-start_time))
