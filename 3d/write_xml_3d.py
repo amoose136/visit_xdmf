7d6625135e88a2b9f8e53f48deaa40b4961aba83
Amos Manneschmidt <amoose136@gmail.com>
2016-07-05 13:24:47 -0400

    added all other scalar...maybe (suspect behavior on some zones for some quantities)

--- 3d/write_xml_3d.py
+++ 3d/write_xml_3d.py
@@ -12,6 +12,7 @@ except:
 try:
 	#Most likly to fail part. On failure, go to exception.
 	import h5py
+
 	#Robustly import an xml writer/parser
 	try:
 		from lxml import etree as et
@@ -47,12 +48,15 @@ try:
 
 	hf = h5py.File(filename,'r')
 	dims=[]
-	#appends the first element of 'array_dimensions' -> 542
+
+	#appends the first element of 'array_dimensions' -> 542 (722 in 3d set)
 	dims.append(hf['mesh']['array_dimensions'][0]) #dims[0]=722
-	#appends the first element of 'array_dimensions' -> 180
+	#appends the second element of 'array_dimensions' -> 180 (8 in 3d set)
 	dims.append(hf['mesh']['array_dimensions'][1]) #dims[1]=8
+	#appends the third element of 'array_dimensions' -> 1 (16 in 3d set)
 	dims.append(hf['mesh']['array_dimensions'][2]) #dims[2]=16
-	dimstr = str(dims[2]/hf['mesh']['nz_hyperslabs'].value)+" "+str(dims[1])+" "+str(dims[0])#dimstr=""
+	dimstr = str(dims[2])+" "+str(dims[1])+" "+str(dims[0])
+	dimstr_sub = str(dims[2]/hf['mesh']['nz_hyperslabs'].value)+" "+str(dims[1])+" "+str(dims[0])#dimstr=""
 	# create xdmf element
 
 	# create Domain element
@@ -60,55 +64,65 @@ try:
 	domain = et.SubElement(xdmf,"Domain")
 
 	grid = {'Hydro':et.SubElement(domain,"Grid",Name="Hydro"),
-			}#'Abundance':et.SubElement(domain,"Grid",Name="Abundance")}
+			'Abundance':et.SubElement(domain,"Grid",Name="Abundance")}
 	et.SubElement(grid['Hydro'],"Topology",TopologyType="3DRectMesh",NumberOfElements=str(dims[2]+1)+" "+str(dims[1]+1)+" "+str(dims[0]+1))
 	geometry = et.SubElement(grid['Hydro'],"Geometry",GeometryType="VXVYVZ")
 	et.SubElement(geometry,"DataItem",Dimensions=str(dims[0]+1),NumberType="Float",Precision="8",Format="HDF").text = filename + ":/mesh/x_ef"
 	et.SubElement(geometry,"DataItem",Dimensions=str(dims[1]+1),NumberType="Float",Precision="8",Format="HDF").text = filename + ":/mesh/y_ef"
 	et.SubElement(geometry,"DataItem",Dimensions=str(dims[2]+1),NumberType="Float",Precision="8",Format="HDF").text = filename + ":/mesh/z_ef"
 	et.SubElement(grid['Hydro'],"Time",Value=str(hf['mesh']['time'].value-hf['mesh']['t_bounce'].value))
-	# et.SubElement(grid['Abundance'],"Topology",Reference="/Xdmf/Domain/Grid[1]/Topology[1]")
-	# et.SubElement(grid['Abundance'],"Geometry",Reference="/Xdmf/Domain/Grid[1]/Geometry[1]")
+	et.SubElement(grid['Abundance'],"Topology",Reference="/Xdmf/Domain/Grid[1]/Topology[1]")
+	et.SubElement(grid['Abundance'],"Geometry",Reference="/Xdmf/Domain/Grid[1]/Geometry[1]")
 
 	storage_names = { 
 		"Entropy":"entropy",
-	# 	"v_radial":"u_c",
-	# 	"v_theta":"v_c",
-	# 	"v_phi":"w_c",
-	# 	"BruntViasala_freq":"wBVMD",
-	# 	"Electron_fraction":"ye_c",
-	# 	"Gravity_phi":"grav_x_c",
-	# 	"Gravity_r":"grav_y_c",
-	# 	"Gravity_theta":"grav_z_c",
-	# 	"Lepton_fraction":"ylep",
-	# 	"Mach_number":"v_csound",
-	# 	"Neutrino_Heating_rate":"dudt_nu",
-	# 	"Nuclear_Heating_rate":"dudt_nuc",
-	# 	"Pressure":"press",
-	# 	"Temperature":"t_c",
-	# 	# "mean_A":"",#computed quantity to be added later
-	# 	"nse_flag":"e_int",
+		"v_radial":"u_c",
+		"v_theta":"v_c",
+		"v_phi":"w_c",
+		"BruntViasala_freq":"wBVMD",
+		"Electron_fraction":"ye_c",
+		"Gravity_phi":"grav_x_c",
+		"Gravity_r":"grav_y_c",
+		"Gravity_theta":"grav_z_c",
+		"Lepton_fraction":"ylep",
+		"Mach_number":"v_csound",
+		"Neutrino_Heating_rate":"dudt_nu",
+		"Nuclear_Heating_rate":"dudt_nuc",
+		"Pressure":"press",
+		"Temperature":"t_c",
+		# "mean_A":"",#computed quantity to be added later
+		"nse_flag":"e_int",
 	}
+	n_hyperslabs = hf['mesh']['nz_hyperslabs'].value
+	function_str="JOIN("
+	for n in range(1, n_hyperslabs+1):
+			function_str+="$"+str(n)
+			if n!=n_hyperslabs:
+				function_str+=" ; "
+			else:
+				function_str+=")"
 	for name in storage_names:
-		at = et.SubElement(grid['Hydro'],"Attribute",Name=name,AttributeType="Scalar",Center="Cell",Dimensions=str(dims[2])+" "+str(dims[1])+" "+str(dims[0]))
-		# col=et.SubElement(at,"DataItem",ItemType="Collection",NumberType="Float",Precision="8")
-		for n in range(1, hf['mesh']['nz_hyperslabs'].value+1):
-			et.SubElement(at,"DataItem",Dimensions=dimstr,NumberType="Float",Precision="8",Format="HDF").text= filename[:-4] + str(n) + ".h5:/fluid/" + storage_names[name]
+		at = et.SubElement(grid['Hydro'],"Attribute",Name=name,AttributeType="Scalar",Center="Cell",Dimensions=dimstr)
+		fun = et.SubElement(at,"DataItem",ItemType="Function", Function=function_str,Dimensions=dimstr)
+		for n in range(1, n_hyperslabs+1):
+			et.SubElement(fun,"DataItem",Dimensions=dimstr_sub,NumberType="Float",Precision="8",Format="HDF").text= filename[:-4] + str(n) + ".h5:/fluid/" + storage_names[name]
 
-	# for i,name in enumerate(hf['abundance']['a_name']):
-		# if re.findall('\D\d',name):
-			# element_name=re.sub('\d','',name)
-			# name=re.sub('\D','',name) #find the transition between elements name and number
-			# if not grid.has_key('Abundance'+'/'+element_name):
-	# 			grid['Abundance'+'/'+element_name]=et.SubElement(domain,"Grid",Name='Abundance'+'/'+element_name,GridType="Uniform")
-	# 			et.SubElement(grid['Abundance'+'/'+element_name],"Topology",Reference="/Xdmf/Domain/Grid[1]/Topology[1]")
-	# 			et.SubElement(grid['Abundance'+'/'+element_name],"Geometry",Reference="/Xdmf/Domain/Grid[1]/Geometry[1]")
-	# 		attribute=et.SubElement(grid['Abundance'+'/'+element_name],"Attribute",Name=name,AttributeType="Scalar",Center="Cell")
-	# 	else:
-	# 		attribute=et.SubElement(grid['Abundance'],"Attribute",Name=name,AttributeType="Scalar",Center="Cell")
-	# 	dataElement = et.SubElement(attribute,"DataItem", ItemType="HyperSlab", Dimensions=dimstr, Type="HyperSlab")
-	# 	et.SubElement(dataElement,"DataItem",Dimensions="3 4",Format="XML").text="0 0 0 "+str(i)+" 1 1 1 1 1 "+dimstr+" 1"
-	# 	et.SubElement(dataElement,"DataItem",Dimensions="1 "+dimstr+" 161",Precisions="8",Format="HDF").text=filename+":/abundance/xn_c"
+	for i,name in enumerate(hf['abundance']['a_name']):
+		if re.findall('\D\d',name):
+			element_name=re.sub('\d','',name)
+			name=re.sub('\D','',name) #find the transition between elements name and number
+			if not grid.has_key('Abundance'+'/'+element_name):
+				grid['Abundance'+'/'+element_name]=et.SubElement(domain,"Grid",Name='Abundance'+'/'+element_name,GridType="Uniform")
+				et.SubElement(grid['Abundance'+'/'+element_name],"Topology",Reference="/Xdmf/Domain/Grid[1]/Topology[1]")
+				et.SubElement(grid['Abundance'+'/'+element_name],"Geometry",Reference="/Xdmf/Domain/Grid[1]/Geometry[1]")
+			attribute=et.SubElement(grid['Abundance'+'/'+element_name],"Attribute",Name=name,AttributeType="Scalar",Center="Cell")
+		else:
+			attribute=et.SubElement(grid['Abundance'],"Attribute",Name=name,AttributeType="Scalar",Center="Cell")
+		fun = et.SubElement(attribute,"DataItem",ItemType="Function", Function=function_str,Dimensions=dimstr)
+		for n in range(1, n_hyperslabs+1):
+			dataElement = et.SubElement(fun,"DataItem", ItemType="HyperSlab", Dimensions=dimstr_sub, Type="HyperSlab")
+			et.SubElement(dataElement,"DataItem",Dimensions="3 4",Format="XML").text="0 0 0 "+str(i)+" 1 1 1 1 "+dimstr_sub+" 1"
+			et.SubElement(dataElement,"DataItem",Dimensions=dimstr_sub+" 17",Precisions="8",Format="HDF").text=filename[:-4] + str(n)+".h5:/abundance/xn_c"
 	
 	# Write document tree to file
 	f=open(filename[:-3]+'.xmf','w')
