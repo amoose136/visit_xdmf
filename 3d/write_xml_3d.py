99c5a9ee782d8a5777fad6e9efd9ca84612d9ab4
Amos Manneschmidt <amoose136@gmail.com>
2016-07-11 17:29:04 -0400

    fixing grid issues for larger set, aka hyperslab hell

--- 3d/write_xml_3d.py
+++ 3d/write_xml_3d.py
@@ -3,10 +3,11 @@ start_time = time.time()
 import sys
 import os
 import subprocess as sp
-visit_path=sp.check_output('which visit',shell=True).rstrip()[:-9]
+from pdb import set_trace as br
+# visit_path=sp.check_output('which visit',shell=True).rstrip()[:-9]
 sys.path.append('/sw/redhat6/visit/current/linux-x86_64/lib/site-packages/')
 sys.path.append('/Applications/VisIt.app/Contents/Resources/2.10.2/darwin-x86_64/lib/site-packages')
-os.environ['DYLD_LIBRARY_PATH']
+# os.environ['DYLD_LIBRARY_PATH']
 try:
 	import visit
 	import visit_utils
@@ -17,7 +18,6 @@ except ImportError:
 	print('Please make sure visit\'s /lib/site-packages directory is in $PYTHONPATH')
 	print ('	( /path/to/visit/VersionNumber/platform/lib/site-packages )')
 	sys.exit()
-visit.Launch()
 try:
 	filename=sys.argv[1]
 except:
@@ -64,51 +64,57 @@ try:
 
 	hf = h5py.File(filename,'r')
 	dims=[]
-
-	#appends the first element of 'array_dimensions' -> 542 (722 in 3d set)
-	dims.append(hf['mesh']['array_dimensions'][0]) #dims[0]=722
-	#appends the second element of 'array_dimensions' -> 180 (8 in 3d set)
-	dims.append(hf['mesh']['array_dimensions'][1]) #dims[1]=8
-	#appends the third element of 'array_dimensions' -> 1 (16 in 3d set)
-	dims.append(hf['mesh']['array_dimensions'][2]) #dims[2]=16
-	dimstr = str(dims[2])+" "+str(dims[1])+" "+str(dims[0])
-	dimstr_sub = str(dims[2]/hf['mesh']['nz_hyperslabs'].value)+" "+str(dims[1])+" "+str(dims[0])#dimstr=""
+	for dim in hf['mesh']['array_dimensions']:
+		dims.append(dim)
+	extents = []  #dimensions actually used
+	extents.append(hf['mesh']['radial_index_bound'][1])
+	extents.append(hf['mesh']['theta_index_bound'][1])
+	extents.append(hf['mesh']['phi_index_bound'][1])
+	extents_sub = str(extents[2]/hf['mesh']['nz_hyperslabs'].value)+" "+str(extents[1])+" "+str(extents[0])
+	dimstr = ' '.join([str(x) for x in dims[::-1]])
+	extents_str = ' '.join([str(x) for x in extents[::-1]])
+	dimstr_sub = str(dims[2]/hf['mesh']['nz_hyperslabs'].value)+" "+str(dims[1])+" "+str(dims[0])
 	# create xdmf element
-
-	# create Domain element
-	xdmf = et.Element("Xdmf",Version="2.0")
+	xdmf = et.Element("Xdmf", Version="2.0")
+	# br()
+	# create Domain element	xdmf = et.Element("Xdmf",Version="2.0")
 	domain = et.SubElement(xdmf,"Domain")
 
 	grid = {'Hydro':et.SubElement(domain,"Grid",Name="Hydro"),
 			'Abundance':et.SubElement(domain,"Grid",Name="Abundance")}
-	et.SubElement(grid['Hydro'],"Topology",TopologyType="3DRectMesh",NumberOfElements=str(dims[2]+1)+" "+str(dims[1]+1)+" "+str(dims[0]+1))
+	et.SubElement(grid['Hydro'],"Topology",TopologyType="3DRectMesh",NumberOfElements=' '.join([str(x+1) for x in extents[::-1]]))
 	geometry = et.SubElement(grid['Hydro'],"Geometry",GeometryType="VXVYVZ")
-	et.SubElement(geometry,"DataItem",Dimensions=str(dims[0]+1),NumberType="Float",Precision="8",Format="HDF").text = filename + ":/mesh/x_ef"
-	et.SubElement(geometry,"DataItem",Dimensions=str(dims[1]+1),NumberType="Float",Precision="8",Format="HDF").text = filename + ":/mesh/y_ef"
-	et.SubElement(geometry,"DataItem",Dimensions=str(dims[2]+1),NumberType="Float",Precision="8",Format="HDF").text = filename + ":/mesh/z_ef"
+	coords=["x_ef","y_ef","z_ef"]
+	for n,coord_name in enumerate(coords):
+		#hyperslab=hs
+		hyperslab = et.SubElement(geometry, "DataItem",Dimensions=str(extents[n]+1),ItemType="HyperSlab",Type="HyperSlab")
+		et.SubElement(hyperslab,"DataItem",Dimensions="3 1",Format="XML").text="0 1 "+str(extents[n]+1)
+		et.SubElement(hyperslab,"DataItem",Dimensions=str(hf['mesh'][coord_name].size),NumberType="Float",Precision="8",Format="HDF").text = filename + ":/mesh/" + coord_name
+		
 	et.SubElement(grid['Hydro'],"Time",Value=str(hf['mesh']['time'].value-hf['mesh']['t_bounce'].value))
 	et.SubElement(grid['Abundance'],"Topology",Reference="/Xdmf/Domain/Grid[1]/Topology[1]")
 	et.SubElement(grid['Abundance'],"Geometry",Reference="/Xdmf/Domain/Grid[1]/Geometry[1]")
 
 	storage_names = { 
 		"Entropy":"entropy",
-		"v_radial":"u_c",
-		"v_theta":"v_c",
-		"v_phi":"w_c",
-		"BruntViasala_freq":"wBVMD",
-		"Electron_fraction":"ye_c",
-		"Gravity_phi":"grav_x_c",
-		"Gravity_r":"grav_y_c",
-		"Gravity_theta":"grav_z_c",
-		"Lepton_fraction":"ylep",
-		"Mach_number":"v_csound",
-		"Neutrino_Heating_rate":"dudt_nu",
-		"Nuclear_Heating_rate":"dudt_nuc",
-		"Pressure":"press",
-		"Temperature":"t_c",
+		# "v_radial":"u_c",
+		# "v_theta":"v_c",
+		# "v_phi":"w_c",
+		# "BruntViasala_freq":"wBVMD",
+		# "Electron_fraction":"ye_c",
+		# "Gravity_phi":"grav_x_c",
+		# "Gravity_r":"grav_y_c",
+		# "Gravity_theta":"grav_z_c",
+		# "Lepton_fraction":"ylep",
+		# "Mach_number":"v_csound",
+		# "Neutrino_Heating_rate":"dudt_nu",
+		# "Nuclear_Heating_rate":"dudt_nuc",
+		# "Pressure":"press",
+		# "Temperature":"t_c",
 		# "mean_A":"",#computed quantity to be added later
-		"nse_flag":"e_int",
+		# "nse_flag":"e_int",
 	}
+	# br()
 	n_hyperslabs = hf['mesh']['nz_hyperslabs'].value
 	function_str="JOIN("
 	for n in range(0, n_hyperslabs):
@@ -118,12 +124,14 @@ try:
 			else:
 				function_str+=")"
 	for name in storage_names:
-		at = et.SubElement(grid['Hydro'],"Attribute",Name=name,AttributeType="Scalar",Center="Cell",Dimensions=dimstr)
-		fun = et.SubElement(at,"DataItem",ItemType="Function", Function=function_str,Dimensions=dimstr)
+		at = et.SubElement(grid['Hydro'],"Attribute",Name=name,AttributeType="Scalar",Center="Cell",Dimensions=extents_str)
+		hyperslab = et.SubElement(at,"DataItem",Dimensions=extents_str,ItemType="HyperSlab",Type="HyperSlab")
+		et.SubElement(hyperslab,"DataItem",Dimensions="3 3",Format="XML").text="0 0 0 1 1 1 "+extents_str
+		fun = et.SubElement(hyperslab,"DataItem",ItemType="Function", Function=function_str,Dimensions=dimstr)
 		for n in range(1, n_hyperslabs+1):
 			et.SubElement(fun,"DataItem",Dimensions=dimstr_sub,NumberType="Float",Precision="8",Format="HDF").text= filename[:-5] + str(format(n, '02d')) + ".h5:/fluid/" + storage_names[name]
 
-	for i,name in enumerate(hf['abundance']['a_name']):
+	"""for i,name in enumerate(hf['abundance']['a_name']):
 		if re.findall('\D\d',name):
 			element_name=re.sub('\d','',name)
 			name=re.sub('\D','',name) #find the transition between elements name and number
@@ -139,7 +147,7 @@ try:
 			dataElement = et.SubElement(fun,"DataItem", ItemType="HyperSlab", Dimensions=dimstr_sub, Type="HyperSlab")
 			et.SubElement(dataElement,"DataItem",Dimensions="3 4",Format="XML").text="0 0 0 "+str(i)+" 1 1 1 1 "+dimstr_sub+" 1"
 			et.SubElement(dataElement,"DataItem",Dimensions=dimstr_sub+" 17",Precisions="8",Format="HDF").text=filename[:-5] + str(format(n, '02d'))+".h5:/abundance/xn_c"
-	
+	"""
 	# Write document tree to file
 	f=open(filename[:-3]+'.xmf','w')
 	# if lxml
@@ -147,16 +155,20 @@ try:
 		f.write(et.tostring(xdmf,pretty_print=True,xml_declaration=True,doctype="<!DOCTYPE Xdmf SYSTEM \"Xdmf.dtd\" []>"))
 	#others
 	except:
+		f.close();
+		f=open(filename[:-3]+'.xmf','w')
 		print("Writing "+filename+".xmf with improvised \"pretty print\"")
 		def prettify(elem):
 			rough_string = et.tostring(elem, 'utf-8')
 			reparsed = md.parseString(rough_string)
 			t = reparsed.toprettyxml(indent="\t")
+			t = ''.join(t.splitlines(True)[1:]) #removes extra doc declaration
 			return t
 		f.write("<?xml version='1.0' encoding='ASCII'?>\n<!DOCTYPE Xdmf SYSTEM \"Xdmf.dtd\" []>\n")
 		f.close()
 		f=open(filename[:-3]+'.xmf','a')
 		f.write(prettify(xdmf))
+
 	f.close()
 	print("--- XMF file created in %s seconds ---" % (time.time()-start_time))
 except ImportError:
@@ -164,7 +176,7 @@ except ImportError:
 		print("Trying to run under reloaded modules")
 		try:
 			sp.call(["module unload PE-intel python;module load PE-gnu python python_h5py"],shell=True)
-			sp.call(["module unload PE-intel python;module load PE-gnu python python_h5py;python write_xml.py "+filename],shell=True)
+			sp.call(["module unload PE-intel python;module load PE-gnu python python_h5py;python write_xml_3d.py "+filename],shell=True)
 		except:
 			#redo the offending call so the error can display
 			sp.call(["module unload PE-intel python;module load PE-gnu python python_h5py"],shell=True)
