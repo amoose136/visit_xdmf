6cd81a67a54967cd66340b572605c35b30b258bd
Amos Manneschmidt <amoose136@gmail.com>
2016-08-31 11:04:34 -0400

    leftover changes

--- 3d/write_xml_3d.py
+++ 3d/write_xml_3d.py
@@ -148,7 +148,9 @@ if __name__ == '__main__':
 		############################################################################################################################################################################################
 		# compute luminosity auxilary variabes
 		if args.aux:
+			qprint("--- Elapased time: %s seconds ---" % (time.time()-old_time))
 			qprint("Creating derived values")
+			qprint("Reading in radiation data from HDF5 files")
 			n_groups=hf['radiation']['raddim'][0]
 			n_species=hf['radiation']['raddim'][1]
 			energy_edge=hf['radiation']['unubi'].value
@@ -171,34 +173,37 @@ if __name__ == '__main__':
 			aux_hf.create_group("/mesh") #or do nothing if exists
 			######## Compute E_RMS_array (size N_species) of arrays (size N_groups) ##############
 			# # initialize variables for parallel loop
-			# psi0_c=hf['radiation']['psi0_c'] 
+			qprint("Done --- Total elapsed time: %s seconds ---" % (time.time()-old_time))
+			qprint("Creating E_RMS_values:")
+			psi0_c=hf['radiation']['psi0_c'] 
 			
-			# E_RMS_array=np.empty((slices,n_species,dims[2],dims[1],dims[0]))
-			# ji=0
+			E_RMS_array=np.empty((slices,n_species,dims[2],dims[1],dims[0]))
 			num_cores=min(16,mp.cpu_count())
-			# def compute_E_RMS_array(sl):	
-			# 	sl+=1
-			# 	i=sl
-			# 	if args.repeat:
-			# 		i=1
-			# 	qprint("Computing E_RMS_[1.."+str(n_species)+"] for slice "+str(sl)+" from "+re.sub("\d\d\.h5",str(format(i, '02d'))+'.h5',re.sub("\d\d_pro\.h5",str(format(i, '02d'))+'_pro.h5',filename)))
-			# 	temp_hf= h5py.File(re.sub("\d\d\.h5",str(format(i, '02d'))+'.h5',re.sub("\d\d_pro\.h5",str(format(i, '02d'))+'_pro.h5',filename)),'r')
-			# 	psi0_c=temp_hf['radiation']['psi0_c'][:]
-			# 	row=np.empty((n_species,dims[2]/n_hyperslabs,dims[1],dims[0]))
-			# 	for n in range(0,n_species):
-			# 		numerator=np.sum(psi0_c[:,:,:,n]*e5de,axis=3)
-			# 		denominator=np.sum(psi0_c[:,:,:,n]*e3de,axis=3)
-			# 		row[n][:][:][:]=np.sqrt(numerator/(denominator+1e-100))
-			# 	return row
-			# results = Parallel(n_jobs=num_cores)(delayed(compute_E_RMS_array)(sl) for sl in range(0,n_hyperslabs))
-			# #concatenate together the member of each array within E_RMS_ARRAY and write to auxilary HDF file
-			# qprint("Concatenating E_RMS_[0.."+str(n_species)+"] results...")
-			# E_RMS_array=np.hstack(results)
-			# for n in range(0,n_species):
-			# 	qprint("Writing E_RMS_"+str(n)+" out to file")
-			# 	aux_hf.create_dataset("/radiation/E_RMS_"+str(n),data=E_RMS_array[n])
-			# del E_RMS_array, results
-			
+			def compute_E_RMS_array(sl):	
+				sl+=1
+				i=sl
+				if args.repeat:
+					i=1
+				qprint("	Computing E_RMS_[1.."+str(n_species)+"] for slice "+str(sl)+" from "+re.sub("\d\d\.h5",str(format(i, '02d'))+'.h5',re.sub("\d\d_pro\.h5",str(format(i, '02d'))+'_pro.h5',filename)))
+				temp_hf= h5py.File(re.sub("\d\d\.h5",str(format(i, '02d'))+'.h5',re.sub("\d\d_pro\.h5",str(format(i, '02d'))+'_pro.h5',filename)),'r')
+				psi0_c=temp_hf['radiation']['psi0_c'][:]
+				row=np.empty((n_species,dims[2]/n_hyperslabs,dims[1],dims[0]))
+				for n in range(0,n_species):
+					numerator=np.sum(psi0_c[:,:,:,n]*e5de,axis=3)
+					denominator=np.sum(psi0_c[:,:,:,n]*e3de,axis=3)
+					row[n][:][:][:]=np.sqrt(numerator/(denominator+1e-100))
+				return row
+			results = Parallel(n_jobs=num_cores)(delayed(compute_E_RMS_array)(sl) for sl in range(0,n_hyperslabs))
+			#concatenate together the member of each array within E_RMS_ARRAY and write to auxilary HDF file
+			qprint("Concatenating E_RMS_[0.."+str(n_species)+"] results...")
+			E_RMS_array=np.hstack(results)
+			qprint("########################################")
+			for n in range(0,n_species):
+				qprint("Writing E_RMS_"+str(n)+" out to file")
+				aux_hf.create_dataset("/radiation/E_RMS_"+str(n),data=E_RMS_array[n])
+			del E_RMS_array, results
+			qprint("########################################")
+			qprint("Done --- Total elapsed time: %s seconds ---" % (time.time()-old_time))
 			# ######## luminosity part ###########
 			qprint("Computing luminosities")
 			psi1_e=hf['radiation']['psi1_e']
@@ -207,18 +212,6 @@ if __name__ == '__main__':
 			cell_area_GRcorrected=4*pi*radius**2/agr_e**4
 			lumin=np.empty((dims[2],dims[1],dims[0]))
 			lumin_array=[]
-			# for species in range(0,n_species):
-			# 	qprint("Computing luminosity for species "+str(species)+":")
-			# 	j=0
-			# 	for sl in range(0,n_hyperslabs):
-			# 		j+=1
-			# 		if args.repeat:
-			# 			sl=0
-			# 		temp_hf= h5py.File(re.sub("\d\d\.h5",str(format(sl+1, '02d'))+'.h5',re.sub("\d\d_pro\.h5",str(format(sl+1, '02d'))+'_pro.h5',filename)),'r')
-			# 		psi1_e=temp_hf['radiation']['psi1_e']
-			# 		qprint("	On slice "+str(j)+" of "+str(n_hyperslabs)+" from "+re.sub("\d\d\.h5",str(format(sl+1, '02d'))+'.h5',re.sub("\d\d_pro\.h5",str(format(sl+1, '02d'))+'_pro.h5',filename)))
-			# 		lumin[sl*6:(sl+1)*6] = np.sum(psi1_e[:,:,:,species]*e3de, axis=3)*np.tile(cell_area_GRcorrected[1:dims[0]+1],(dims[2]/n_hyperslabs,dims[1],1))*(cvel*ecoef*1e-51)
-			# 	lumin_array.append(lumin)
 			def compute_luminosity(species,sl):
 				j=sl
 				if args.repeat:
@@ -232,20 +225,21 @@ if __name__ == '__main__':
 			for species in range(0,n_species):
 				qprint("Computing luminosity for species "+str(species)+":")
 				lumin_array[species] = np.vstack(Parallel(n_jobs=num_cores)(delayed(compute_luminosity)(0,sl) for sl in range(0,n_hyperslabs)))
+			qprint("--- Total elapsed time: %s seconds ---" % (time.time()-old_time))
 			qprint("########################################")
 			for n,lumin in enumerate(lumin_array):
 				qprint("Writing luminosity species "+str(n)+" to auxilary hdf file")
 				aux_hf.create_dataset("/radiation/Luminosity_"+str(n),data=lumin)
-			# now stack mask for YY grid:
 			qprint("########################################")
+			# now stack mask for YY grid:
 			qprint("Creating On_grid_mask")
 			mask_slice = hf['mesh']['ongrid_mask'].value
 			mask = np.dstack( mask_slice for i in range(0,extents[0]))
 			del mask_slice
 			qprint("Writing On_grid_mask to auxillary file")
 			aux_hf.create_dataset("/mesh/mask",data=mask)
-			
 			aux_hf.close()
+			qprint("Done --- Total elapsed time: %s seconds ---" % (time.time()-old_time))
 			# del aux_hf,lumin_array,lumin,mask
 		############################################################################################################################################################################################
 		extents[2]=extents[2]/n_hyperslabs*slices
@@ -386,6 +380,7 @@ if __name__ == '__main__':
 
 		############################################################################################################################################################################################
 		# Write document tree to file
+		qprint("Writing xml to file")
 		try:
 			# explode filename into list
 			file_directory=''
